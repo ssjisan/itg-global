62702edba71e2a5bede83ca7c0763b46d15f9eee
ssjisan <ssjisan.dev@gmail.com>
2025-08-29 02:19:03 +0600

    Remove

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,11 @@
 import { Box } from "@mui/material";
 import HeroSection from "../Components/Home/HeroSection";
-// import ChatwootWidget from "../Components/Chatwoot/ChatwootWidget";
 
 export default function Home() {
 
   return (
     <Box>
       <HeroSection />
-      {/* <ChatwootWidget/> */}
     </Box>
   );
 }
